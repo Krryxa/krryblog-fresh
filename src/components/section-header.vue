fd1082ed5995de7920544262e2ac1d862c3e0dfd
Krryxa <krryxq@163.com>
2021-10-09 18:00:01 +0800

    feat: add link about common

---
+++ src/components/section-header.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+defineProps(['title', 'description'])
+</script>
+
+<template>
+  <div class="header">
+    <h1>
+      <span class="e-left">『</span>
+      <span class="text">{{ title }}</span>
+      <span class="e-right">』</span>
+    </h1>
+    <h4>{{ description }}</h4>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 96px 0 28px;
+  text-align: center;
+  background: url(../assets/pic/category.jpg) no-repeat;
+  background-size: cover;
+
+  h1 {
+    position: relative;
+    margin: 0 auto;
+    margin-bottom: 10px;
+    font-size: 30px;
+    font-weight: 400;
+    color: rgba(255, 255, 255, 0.89);
+    text-shadow: 0 4px 8px rgba(7, 17, 27, 0.4);
+    letter-spacing: 0.1em;
+
+    .e-left {
+      position: relative;
+      top: -12px;
+      left: 2px;
+    }
+
+    .e-right {
+      position: relative;
+      top: -10px;
+      right: 2px;
+    }
+
+    .text {
+      display: inline-block;
+      max-width: 660px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+
+  h4 {
+    font-size: 16px;
+    font-weight: 400;
+    color: rgba(255, 255, 255, 0.54);
+  }
+}
+</style>
--- src/views/blog/detail.vue
+++ src/views/blog/detail.vue
@@ -1,25 +1,68 @@
 <script setup lang="ts">
-import { ref, Ref, getCurrentInstance, computed, watch } from 'vue'
+import { ref, Ref, getCurrentInstance, computed, watch, nextTick } from 'vue'
 import { useRoute } from 'vue-router'
-import { getBlogDetail } from '@/service/api'
+import { getBlogDetail, getLinkOrAbout } from '@/service/api'
 import blogDetail from '@/components/blog-detail.vue'
+import sectionHeader from '@/components/section-header.vue'
 
 interface BlogItemType {
   [propName: string]: string | number
 }
+interface ObjectType {
+  [propName: string]: Object | string
+}
+interface LinkOrAboutMapType {
+  [propName: string]: ObjectType
+}
 
+const route = useRoute()
 let blog: Ref<BlogItemType> = ref({})
 let status = 200
+let reFresh = ref(true)
+let reFreshDetail = ref(true)
+let beforeRouterName = ref(route.name)
+
 const isNoBlog = computed(() => status === 404)
-const route = useRoute()
+
+let description: Ref<string | Object> = ref('')
+const routerName = computed(() => route.name as string)
+
+const linkOrAboutMap: LinkOrAboutMapType = {
+  link: { params: { title: '友情链接' }, description: 'Website link' },
+  about: { params: { title: '关于我' }, description: 'About me' }
+}
+const beforeCurrentSame = computed(() => {
+  const linkOrAboutMapKeys = Object.keys(linkOrAboutMap)
+  return (
+    linkOrAboutMapKeys.includes(beforeRouterName.value as string) &&
+    linkOrAboutMapKeys.includes(route.name as string)
+  )
+})
+const isOthers = computed(() =>
+  Object.keys(linkOrAboutMap).includes(routerName.value)
+)
 
 const { proxy }: any = getCurrentInstance()
 const documentTitle = proxy.documentTitle
 const getBlog = async () => {
-  let id = route.params['id']
-  let res: any = await getBlogDetail(id)
+  const id = route.params['id']
+  const res: any = await (isOthers.value
+    ? getLinkOrAbout(linkOrAboutMap[routerName.value].params)
+    : getBlogDetail(id))
   status = res.code
   blog.value = res.result.data
+  // 保证接口数据返回赋值
+  description.value = isOthers.value
+    ? linkOrAboutMap[routerName.value].description
+    : ''
+  reFresh.value = true
+  // 路由前后都是友情链接 or 关于我，则刷新 blog-detail
+  if (beforeCurrentSame.value) {
+    reFreshDetail.value = false // 刷新 detail
+    nextTick(() => {
+      reFreshDetail.value = true
+    })
+  }
   // 404 的标题在 axios 拦截器已经定义
   if (status === 200) {
     document.title = `${blog.value.title} - ${documentTitle}`
@@ -32,14 +75,29 @@ const clearBlog = () => {
   blog.value = {}
 }
 
-watch(route, () => {
-  getBlog()
+watch(route, (to) => {
+  const currentName = to.name as string
+  if ([...Object.keys(linkOrAboutMap), 'blog'].includes(currentName)) {
+    // 路由前后都是友情链接 or 关于我，则不刷新 section-header
+    reFresh.value = beforeCurrentSame.value
+    beforeRouterName.value = to.name as string
+    getBlog()
+  }
 })
 </script>
 
 <template>
   <main v-if="!isNoBlog">
-    <blog-detail :blog="blog" @clearBlog="clearBlog"></blog-detail>
+    <section-header
+      v-if="isOthers && reFresh"
+      :title="blog.title"
+      :description="description"
+    ></section-header>
+    <blog-detail
+      v-if="reFreshDetail"
+      :blog="blog"
+      @clearBlog="clearBlog"
+    ></blog-detail>
   </main>
   <not-found v-else></not-found>
 </template>
