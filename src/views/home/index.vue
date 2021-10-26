1055d0ea60ca297ed13d79b267427d32fd9dd0b2
Krryxa <krryxq@163.com>
2021-10-26 11:19:25 +0800

    feat: category title

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -64,7 +64,12 @@ const getBlogList = async () => {
   // 设置标题
   if (config.value.title) {
     document.title = config.value.title
-      .replace('$param', headerTitle.value)
+      .replace(
+        '$param',
+        config.value?.header?.title
+          ? res.result[config.value.header.title]
+          : headerTitle.value
+      )
       .replace('$documentTitle', documentTitle)
   }
   if (res.code === 200) {
