cf5092614dbc2e405c8a9c9987f51d90c95ac6dc
Krryxa <krryxq@163.com>
2022-06-15 16:55:27 +0800

    feat(data): code optimize

--- src/main.ts
+++ src/main.ts
@@ -48,8 +48,8 @@ router.beforeEach((to, from, next) => {
 })
 
 // 配置页面标题
-router.afterEach((to, from) => {
-  if (to.meta && to.meta.title) {
+router.afterEach((to) => {
+  if (to.meta?.title) {
     document.title = (
       to.meta.cover
         ? to.meta.title
